67df6bff7195b048cfa1cd4b39de33ea0a59e2dc
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2022-02-16 19:15:28 -0300

    finalizando testes automatizados

--- spec/services/project_service_spec.rb
+++ spec/services/project_service_spec.rb
@@ -13,15 +13,23 @@ RSpec.describe 'ProjectService', type: :model do
     end
 
     it 'projeto com 1 atividade pendente' do
+      allow(Task).to receive_message_chain(:select, :where, :first) { double('Task', total_tasks: 1, tasks_concluded: 0) }
       expect(@project_service.completed_percent(project_id: 1)).to eq(0)
     end
 
     it 'projeto com 1 atividade encerrada' do
+      allow(Task).to receive_message_chain(:select, :where, :first) { double('Task', total_tasks: 1, tasks_concluded: 1) }
       expect(@project_service.completed_percent(project_id: 1)).to eq(100)
     end
 
-    it 'projeto com atividade(s) pendente(s) e encerrada(s)' do
-      expect(@project_service.completed_percent(project_id: 1)).to eq('100')
+    it 'projeto com 2 atividade(s) pendente(s) e 2 encerrada(s)' do
+      allow(Task).to receive_message_chain(:select, :where, :first) { double('Task', total_tasks: 4, tasks_concluded: 2) }
+      expect(@project_service.completed_percent(project_id: 1)).to eq(50)
+    end
+
+    it 'projeto com 6 atividade(s) pendente(s) e 1 encerrada(s)' do
+      allow(Task).to receive_message_chain(:select, :where, :first) { double('Task', total_tasks: 7, tasks_concluded: 1) }
+      expect(@project_service.completed_percent(project_id: 1).round(2)).to eq(14.29)
     end
   end
 end

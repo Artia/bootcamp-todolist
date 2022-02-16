f199881a770ea19f5b87746c486950ed73ffd126
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2022-02-16 19:20:07 -0300

    centralizando o metodo para mostrar formar de mockar

--- app/services/project_service.rb
+++ app/services/project_service.rb
@@ -16,13 +16,17 @@ class ProjectService < ApplicationService
       # (total_concluded.to_f / total.to_f) * 100
   
       # Calculando pelo sql
-      info_tasks = Task.select("COUNT(*) as total_tasks, SUM(if(state = true, 1, 0)) as tasks_concluded").where(project_id: project_id).first
+      info_tasks = info_tasks(project_id: project_id)
       
       return 0 if info_tasks.total_tasks.to_f.zero?
       (info_tasks.tasks_concluded.to_f / info_tasks.total_tasks.to_f) * 100
     end
 
     private
+
+    def info_tasks(project_id)
+      Task.select("COUNT(*) as total_tasks, SUM(if(state = true, 1, 0)) as tasks_concluded").where(project_id: project_id).first
+    end
 
     def find_project(project_id:)
       Project.where(id: project_id).first
--- spec/services/project_service_spec.rb
+++ spec/services/project_service_spec.rb
@@ -28,7 +28,8 @@ RSpec.describe 'ProjectService', type: :model do
     end
 
     it 'projeto com 6 atividade(s) pendente(s) e 1 encerrada(s)' do
-      allow(Task).to receive_message_chain(:select, :where, :first) { double('Task', total_tasks: 7, tasks_concluded: 1) }
+      #allow(Task).to receive_message_chain(:select, :where, :first) { double('Task', total_tasks: 7, tasks_concluded: 1) }
+      allow(@project_service).to receive(:info_tasks).with(project_id: @project_id).and_return(double('Task', total_tasks: 7, tasks_concluded: 1))
       expect(@project_service.completed_percent(project_id: 1).round(2)).to eq(14.29)
     end
   end

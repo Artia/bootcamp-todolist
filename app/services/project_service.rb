c6ad7e488fa6353591ddb516e2d7bdf688a8e05b
efsiqueira <edu.fss14@gmail.com>
2022-04-07 16:01:28 -0300

    Initial commit

---
+++ app/services/project_service.rb
@@ -0,0 +1,16 @@
+class ProjectService < ApplicationService
+    def update_percent_complete(project_id:)
+        find_project(project_id: project_id).update(completed_percent: complete_percentage(project_id: project_id))
+    end
+    
+    def complete_percentage(project_id:)
+        info_tasks = Task.select("COUNT(*) as total_tasks, SUM(if(state = true, 1, 0)) as task_concluded").where(project_id: project_id).first
+        (info_tasks.task_concluded.to_f / info_tasks.total_tasks.to_f) * 100
+    end
+
+    private
+
+    def find_project(project_id:)
+        Project.find_by(id: project_id)    
+    end
+end

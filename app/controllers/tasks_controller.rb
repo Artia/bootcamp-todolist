2e8bb8c3053ff296b91ecd1bbf69704f972ae3af
GABRIEL OLIVEIRA <GABRIEL.OLIVEIRA@EUAX.COM.BR>
2022-04-25 19:26:21 -0300

    Configuração prévia com alterações para inserção de tarefas relacionadas individualmente a cada projeto

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,10 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[ show edit update destroy ]
+  before_action :set_project
 
   # GET /tasks or /tasks.json
   def index
-    @tasks = Task.all
+    @tasks = @project.tasks
   end
 
   # GET /tasks/1 or /tasks/1.json
@@ -22,10 +23,11 @@ class TasksController < ApplicationController
   # POST /tasks or /tasks.json
   def create
     @task = Task.new(task_params)
+    @task.project_id = @project.id
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to task_url(@task), notice: "Task was successfully created." }
+        format.html { redirect_to project_task_url(@project, @task), notice: "Task was successfully created." }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +40,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to task_url(@task), notice: "Task was successfully updated." }
+        format.html { redirect_to project_task_url(@project, @task), notice: "Task was successfully updated." }
         format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +54,7 @@ class TasksController < ApplicationController
     @task.destroy
 
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: "Task was successfully destroyed." }
+      format.html { redirect_to project_tasks_url(@project), notice: "Task was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -65,6 +67,10 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:title, :date_start, :date_end, :state, :project_id)
+      params.require(:task).permit(:title, :date_start, :date_end, :state)
+    end
+
+    def set_project
+      @project = Project.find_by(id: params[:project_id].to_i)
     end
 end

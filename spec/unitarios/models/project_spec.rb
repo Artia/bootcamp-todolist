914c2c2e300e1772c8e64863d10c6d92909cbb53
GABRIEL OLIVEIRA <GABRIEL.OLIVEIRA@EUAX.COM.BR>
2022-04-26 21:12:18 -0300

    Primeiros testes com RSpec

--- spec/unitarios/models/project_spec.rb
+++ spec/unitarios/models/project_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
-# DESAFIO
 RSpec.describe Project, type: :model do
+
   describe 'associações' do
+    it { is_expected.to have_many(:tasks).dependent(:destroy) }
   end
 
   describe 'validações' do
+    describe 'title' do
+      it { is_expected.to validate_presence_of(:title) }
+      it { is_expected.to validate_length_of(:title).is_at_most(255) } 
+    end
   end
 end
